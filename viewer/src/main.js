5cabcc1c11ab4c55050fd2632a4809c66a16791b
dogtwelve <x3191@hotmail.com>
2015-05-20 13:22:26 +0800

    !scrollview displaying pages

--- viewer/src/main.js
+++ viewer/src/main.js
@@ -8,6 +8,8 @@ define(function(require, exports, module) {
     var Utility = require('famous/utilities/Utility');
     var Modifier  = require('famous/core/Modifier');
     var Draggable = require('famous/modifiers/Draggable');
+    var Scrollview = require("famous/views/Scrollview");
+    var ContainerSurface = require("famous/surfaces/ContainerSurface");
 
     // create the main context
     var mainContext = Engine.createContext();
@@ -15,7 +17,7 @@ define(function(require, exports, module) {
     // your app here
     mainContext.setPerspective(1000);
 
-    var stageView = new StageView();
+    //var stageView = new StageView();
     var director = new Director();
 
     var actorExampleText = '<pre>\'Presenter Name\': { \n    type: \'html\', \n    content: \'BOB OWEN\', \n    properties: { \n        backfaceVisibility: \'visible\', \n        fontSize: \'6.5em\', \n        textAlign: \'center\' \n    }, \n    size: [700, 140], \n    position: [\'75%\', \'50%\'], \n    classes: [\'z2\'] \n}, \n</pre>';
@@ -971,24 +973,73 @@ define(function(require, exports, module) {
             page = dataContent[0];
         }
         console.log("dataContent:" +  dataContent);
+
+        var max_page_width = page.width;
+
+        var max_page_height = page.height;
+
+        var subpage_counts = 3;//page.pageIDs.length;
+
         var pageView = new StageView({
             pageId:  page.id_,
             size: [page.width, page.height]
         });
+
         var originModifier = new Modifier({
-            size:[page.width, undefined],
+            size:[max_page_width, max_page_height],
             origin: [0.5, 0],
             align: [0.5, 0]
         });
 
-
         director.populateStage(pageView, page.nodes);
+
+        //mainContext.add(originModifier).add(pageView);
+
+
+
 
         //var draggable = new Draggable();
         //draggable.subscribe(pageView.scrollRecieverSurface);
         //mainContext.add(originModifier).add(draggable).add(pageView);
 
-        mainContext.add(originModifier).add(pageView);
+        var pageViews = [];
+
+        pageViews.push(pageView)
+
+        for(var subpageIdx = 0 ; subpageIdx < subpage_counts; subpageIdx ++ ) {
+            var subpage = page;
+            var subpageView = new StageView({
+                pageId:  page.id_,
+                size: [page.width, page.height]
+            });
+
+            director.populateStage(subpageView, subpage.nodes);
+            pageViews.push(subpageView);
+
+            max_page_height +=  subpage.height;
+        }
+
+        var scrollview = new Scrollview();
+        scrollview.sequenceFrom(pageViews);
+
+        var contextSize = mainContext.getSize();
+        var draggable = new Draggable({
+                xRange: [0, 0],
+                yRange: [- (max_page_height - contextSize[1]), 0]
+            }
+        );
+
+        for(var pageView in pageViews) {
+            draggable.subscribe(pageViews[pageView].scrollRecieverSurface);
+        }
+
+        var originScrollviewModifier = new Modifier({
+            size:[max_page_width, max_page_height],
+            origin: [0.5, 0],
+            align: [0.5, 0]
+        });
+
+        mainContext.add(originScrollviewModifier).add(draggable).add(scrollview);
 
     }.bind(this));
 

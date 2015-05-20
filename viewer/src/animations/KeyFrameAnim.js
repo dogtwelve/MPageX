953f4c42af39746b6ab237d6340a8759b97360be
dogtwelve <x3191@hotmail.com>
2015-05-21 06:52:21 +0800

    update path and so on by keyframe time

--- viewer/src/animations/KeyFrameAnim.js
+++ viewer/src/animations/KeyFrameAnim.js
@@ -11,23 +11,30 @@ define(function (require, exports, module) {
     function KeyFrameAnim(actor, totalTime, keyFrames){
         this.actor = actor;
         this.keyFrames = keyFrames;
+        this.keyFramesCount = keyFrames.length;
         this.elapsed = 30;
         this.curAnimFrameIdx = 0;
         this.curAnimTime = 0;
+        this.nextFrameTime = 0;
         this.totalAnimTime = totalTime * 1000;
+        this.initOffsetX = 0;
+        this.initOffsetY = 0;
     };
 
     KeyFrameAnim.prototype.activeAnim = function() {
-        this.animTimer = Timer.setInterval(function(){this.updateAnim();}.bind(this), this.elapsed);
         this.readyAnim();
+        this.animTimer = Timer.setInterval(function(){this.updateAnim();}.bind(this), this.elapsed);
     };
 
     KeyFrameAnim.prototype.readyAnim = function() {
+        this.actor.createDisplacementModifier();
+        this.resetAnim();
+    };
+
+    KeyFrameAnim.prototype.resetAnim = function() {
         this.curAnimTime = 0;
-        this.curAnimFrameIdx = 0;
-        this.x_path = new MotionPath();
-        this.y_path = new MotionPath();
-        this.readyDisplacement();
+        this.curAnimFrameIdx = -1;
+        this.goNextKeyFrame();
     };
 
     KeyFrameAnim.prototype.readyDisplacement = function() {
@@ -36,59 +43,51 @@ define(function (require, exports, module) {
         var ctrl_ptsB = [];
         var node_count = this.keyFrames.length;
 
-        var startFrameDesc = this.keyFrames[0];
+        var curFrameDesc = this.keyFrames[this.curAnimFrameIdx];
+        var nextFrameDesc = this.keyFrames[this.curAnimFrameIdx + 1];
 
-        var posStartX = startFrameDesc.positionX;
-        var posStartY = startFrameDesc.positionY;
-        points.push({
-            x: posStartX,
-            y: posStartY
-        })
+        var posStartX = curFrameDesc.positionX;
+        var posStartY = curFrameDesc.positionY;
+
+        if(this.curAnimFrameIdx === 0) {
+            this.initOffsetX = posStartX;
+            this.initOffsetY = posStartY;
+        }
+
+        var posNextX = nextFrameDesc.positionX;
+        var posNextY = nextFrameDesc.positionY;
 
         var ctrl_ptA = {
-            x: posStartX + startFrameDesc.controlAX,
-            y: posStartY + startFrameDesc.controlAY
+            x: posStartX + curFrameDesc.controlAX,
+            y: posStartY + curFrameDesc.controlAY
         };
 
-        for(var index = 1;  index < node_count; index ++) {
-            ctrl_ptsA.push(ctrl_ptA);
-
-            var keyframeDesc = this.keyFrames[index];
-
-            var posX = keyframeDesc.positionX;
-            var posY = keyframeDesc.positionY;
-            points.push({
-                x: posX,
-                y: posY
-            })
-
-            var ctrl_ptB = {
-                x: posX + keyframeDesc.controlBX,
-                y: posY + keyframeDesc.controlBY
-            };
-
-            ctrl_ptsB.push(ctrl_ptB);
-
-            ctrl_ptA = {
-                x: posX + keyframeDesc.controlAX,
-                y: posY + keyframeDesc.controlAY
-            };
-
+        var ctrl_ptB = {
+            x: posNextX + nextFrameDesc.controlBX,
+            y: posNextY + nextFrameDesc.controlBY
         }
 
         //create two motion paths, one for each dimension. see motionpath.js for properties
-        this.x_path.start = points[0].x;
-        this.y_path.start = points[0].y;
-        for(var pointIdx = 0; pointIdx < node_count - 1; pointIdx ++) {
-            this.x_path.bezierCurveTo(ctrl_ptsA[pointIdx].x, ctrl_ptsB[pointIdx].x, points[pointIdx + 1].x);
-            this.y_path.bezierCurveTo(ctrl_ptsA[pointIdx].y, ctrl_ptsB[pointIdx].y, points[pointIdx + 1].y);
-        }
+        this.x_path = new MotionPath();
+        this.y_path = new MotionPath();
+        this.x_path.start = posStartX;
+        this.y_path.start = posStartY;
+        this.x_path.bezierCurveTo(ctrl_ptA.x, ctrl_ptB.x, posNextX);
+        this.y_path.bezierCurveTo(ctrl_ptA.y, ctrl_ptB.y, posNextY);
 
-        this.actor.createDisplacementModifier();
     };
 
     KeyFrameAnim.prototype.goNextKeyFrame = function() {
+        this.curAnimFrameIdx ++;
+        this.curFrameTime = this.keyFrames[this.curAnimFrameIdx].time * 1000;
 
+        if(this.curAnimFrameIdx < (this.keyFramesCount - 2)) {
+            this.nextFrameTime = this.keyFrames[this.curAnimFrameIdx + 1].time * 1000;
+        } else {
+            this.nextFrameTime = this.totalAnimTime;
+        }
+
+        this.readyDisplacement();
     };
 
     KeyFrameAnim.prototype.stopAnim = function() {
@@ -100,20 +99,26 @@ define(function (require, exports, module) {
 
     KeyFrameAnim.prototype.updateAnim = function() {
         this.curAnimTime += this.elapsed;
+        var isFireNextFrame = false;
 
         if(this.curAnimTime > this.totalAnimTime) {
-            this.curAnimTime = this.totalAnimTime;
+            this.resetAnim();
+            return;
+        } else if(this.curAnimTime >= this.nextFrameTime) {
+            this.curAnimTime = this.nextFrameTime;
+            isFireNextFrame = true;
         }
 
-        var time = this.curAnimTime / this.totalAnimTime;
+        var time = (this.curAnimTime - this.curFrameTime) / (this.nextFrameTime - this.curFrameTime);
         //calculate position on entire path (0.0 - 1.0)
         var pos_x = this.x_path.interpolate(time);
         var pos_y = this.y_path.interpolate(time);
-        this.actor.setDisplacementPos(pos_x, pos_y);
+        this.actor.setDisplacementPos(pos_x - this.initOffsetX, pos_y - this.initOffsetX);
 
         console.log(this.actor.name + ' pos(' + pos_x + ','+ pos_y + ')');
-        if(this.curAnimTime >= this.totalAnimTime) {
-            this.curAnimTime = 0;
+
+        if(isFireNextFrame) {
+            this.goNextKeyFrame();
         }
     };
 

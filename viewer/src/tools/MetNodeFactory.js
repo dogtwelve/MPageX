df8a6006c91e5010cccea1f4733eb9ce8ee4aeca
dogtwelve <x3191@hotmail.com>
2015-05-21 10:00:36 +0800

    videoURL and webNode

--- viewer/src/tools/MetNodeFactory.js
+++ viewer/src/tools/MetNodeFactory.js
@@ -158,10 +158,19 @@ define(function(require, exports, module) {
         }
 
         if(type === "VideoNode") {
+            var videoURL = nodeDescription.videoURL;
             newSurface = new VideoSurface({
                 size: size
             });
             newSurface.setContent("http://developer.qiniu.com/tutorial%2Fqrsbox_video_for_win_web_low_origin.mp4");
+        }
+
+        if(type === "WebNode") {
+            var webUrl = nodeDescription.URL;
+            newSurface = new Surface({
+                size: size
+            });
+            newSurface.setContent(webUrl);
         }
 
         if(newSurface) {
@@ -230,108 +239,108 @@ define(function(require, exports, module) {
         context.closePath();
     }
 
-    MetNodeFactory.prototype.makeMetNodeNew = function(name, nodeDescription, containerSize) {
-        var newSurface;
-
-        if (nodeDescription.zPosition && nodeDescription.properties) {
-            nodeDescription.properties.zPosition = nodeDescription.zPosition;
-        }
-
-        // Make sure size is in pixels.
-        nodeDescription.size = UnitConverter._unitsToPixels(nodeDescription.size, containerSize);
-
-        // Make sure position is in pixels.
-        if (nodeDescription.position) {
-            nodeDescription.position = UnitConverter._unitsToPixels(nodeDescription.position, containerSize);
-        }
-
-
-        var type = nodeDescription.type;
-        var content = nodeDescription.content;
-        var classes = nodeDescription.classes;
-        var properties = nodeDescription.properties;
-        var size = nodeDescription.size;
-        var opacity = nodeDescription.opacity;
-
-        // Ensure backface visibility is set for all new surfaces.
-        if (!classes) {
-            classes = ['backfaceVisibility'];
-        } else {
-            classes.push('backfaceVisibility');
-        }
-
-        if (type === 'html') {
-            newSurface = new Surface({
-                size: size,
-                content: content,
-                properties: properties,
-                classes: classes
-            });
-            //newSurface = new CanvasSurface({
-            //    size: size,
-            //    properties: properties,
-            //    classes: classes
-            //});
-            //newSurface.render = function render() {
-            //
-            //    var ctx = this.getContext('2d');
-            //
-            //    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-            //
-            //    //ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-            //    //ctx.moveTo(10, i);
-            //    //ctx.lineTo(60, i);
-            //    //ctx.stroke();
-            //
-            //    //if (i++ > ctx.canvas.height) i = 0;
-            //
-            //    return this.id;
-            //};
-
-        }
-
-        if (type === 'image') {
-            //newSurface = new BgImageSurface({
-            //    sizeMode: BgImageSurface.SizeMode.ASPECTFILL,
-            //    content: content,
-            //    properties: properties,
-            //    classes: classes
-            //});
-            newSurface = new ImageSurface({
-                    size: size,
-                    content: content,
-                    properties: properties,
-                    classes: classes
-            });
-        }
-
-        var newNode = new MetNodeView({
-            name: name,
-            zPosition: properties.zPosition,
-            opacity: opacity !== undefined ? opacity : 1,
-            containerSize:containerSize
-        });
-
-        newNode.addSurface(newSurface);
-
-        var subMetNodes = nodeDescription.nodes;
-        for(var subMetNodenode in subMetNodes) {
-            var newSubNode = this.makeMetNodeNew(
-                subMetNodes[subMetNodenode].name,
-                subMetNodes[subMetNodenode],
-                nodeDescription.size);
-            newNode.addSubMetNode(newSubNode);
-        }
-
-
-
-
-        this.metNodesFromFactory[name] = newNode;
-
-        newNode.setPositionPixels(nodeDescription.position[0], nodeDescription.position[1]);
-
-        return newNode;
-    };
+    //MetNodeFactory.prototype.makeMetNodeNew = function(name, nodeDescription, containerSize) {
+    //    var newSurface;
+    //
+    //    if (nodeDescription.zPosition && nodeDescription.properties) {
+    //        nodeDescription.properties.zPosition = nodeDescription.zPosition;
+    //    }
+    //
+    //    // Make sure size is in pixels.
+    //    nodeDescription.size = UnitConverter._unitsToPixels(nodeDescription.size, containerSize);
+    //
+    //    // Make sure position is in pixels.
+    //    if (nodeDescription.position) {
+    //        nodeDescription.position = UnitConverter._unitsToPixels(nodeDescription.position, containerSize);
+    //    }
+    //
+    //
+    //    var type = nodeDescription.type;
+    //    var content = nodeDescription.content;
+    //    var classes = nodeDescription.classes;
+    //    var properties = nodeDescription.properties;
+    //    var size = nodeDescription.size;
+    //    var opacity = nodeDescription.opacity;
+    //
+    //    // Ensure backface visibility is set for all new surfaces.
+    //    if (!classes) {
+    //        classes = ['backfaceVisibility'];
+    //    } else {
+    //        classes.push('backfaceVisibility');
+    //    }
+    //
+    //    if (type === 'html') {
+    //        newSurface = new Surface({
+    //            size: size,
+    //            content: content,
+    //            properties: properties,
+    //            classes: classes
+    //        });
+    //        //newSurface = new CanvasSurface({
+    //        //    size: size,
+    //        //    properties: properties,
+    //        //    classes: classes
+    //        //});
+    //        //newSurface.render = function render() {
+    //        //
+    //        //    var ctx = this.getContext('2d');
+    //        //
+    //        //    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    //        //
+    //        //    //ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    //        //    //ctx.moveTo(10, i);
+    //        //    //ctx.lineTo(60, i);
+    //        //    //ctx.stroke();
+    //        //
+    //        //    //if (i++ > ctx.canvas.height) i = 0;
+    //        //
+    //        //    return this.id;
+    //        //};
+    //
+    //    }
+    //
+    //    if (type === 'image') {
+    //        //newSurface = new BgImageSurface({
+    //        //    sizeMode: BgImageSurface.SizeMode.ASPECTFILL,
+    //        //    content: content,
+    //        //    properties: properties,
+    //        //    classes: classes
+    //        //});
+    //        newSurface = new ImageSurface({
+    //                size: size,
+    //                content: content,
+    //                properties: properties,
+    //                classes: classes
+    //        });
+    //    }
+    //
+    //    var newNode = new MetNodeView({
+    //        name: name,
+    //        zPosition: properties.zPosition,
+    //        opacity: opacity !== undefined ? opacity : 1,
+    //        containerSize:containerSize
+    //    });
+    //
+    //    newNode.addSurface(newSurface);
+    //
+    //    var subMetNodes = nodeDescription.nodes;
+    //    for(var subMetNodenode in subMetNodes) {
+    //        var newSubNode = this.makeMetNodeNew(
+    //            subMetNodes[subMetNodenode].name,
+    //            subMetNodes[subMetNodenode],
+    //            nodeDescription.size);
+    //        newNode.addSubMetNode(newSubNode);
+    //    }
+    //
+    //
+    //
+    //
+    //    this.metNodesFromFactory[name] = newNode;
+    //
+    //    newNode.setPositionPixels(nodeDescription.position[0], nodeDescription.position[1]);
+    //
+    //    return newNode;
+    //};
 
     //MetNodeFactory.prototype.makeMetNode = function(name, type, content, classes, properties, size, opacity, scrollStart, scrollStop) {
     //    var newSurface;

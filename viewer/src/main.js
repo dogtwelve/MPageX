c89b463e039a67301b7969bef5f259876e631110
dogtwelve <x3191@hotmail.com>
2015-05-24 22:47:34 +0800

    scale for screen px

--- viewer/src/main.js
+++ viewer/src/main.js
@@ -4,7 +4,11 @@ define(function(require, exports, module) {
     // import dependencies
     var Engine = require('famous/core/Engine');
     var Utility = require('famous/utilities/Utility');
-    var Modifier  = require('famous/core/Modifier');
+    var Modifier      = require('famous/core/Modifier');
+    var Transform     = require('famous/core/Transform');
+    var View          = require('famous/core/View');
+    var Surface       = require('famous/core/Surface');
+    var ImageSurface  = require('famous/surfaces/ImageSurface');
     var Draggable = require('famous/modifiers/Draggable');
     var Scrollview = require("famous/views/Scrollview");
     var ContainerSurface = require("famous/surfaces/ContainerSurface");
@@ -12,11 +16,64 @@ define(function(require, exports, module) {
     var Director = require('tools/Director');
     var DebugUtils = require('utils/DebugUtils');
 
-    // create the main context
-    var mainContext = Engine.createContext();
+    var context = null;
+    var appDims;
+    function getAppDims(origW, origH){
+        var scaleX = window.innerWidth / origW;
+        var scaleY = window.innerHeight / origH;
 
-    // your app here
-    mainContext.setPerspective(1000);
+        //here we are going to let the bottom of the screen be cut off to allow fit to more
+        //devices
+        var scale = Math.min(scaleX, scaleY);
+        //var scale = 1;
+
+        var appWidth = Math.round(origW * scale);
+        var appHeight = Math.round(origH * scale);
+
+        return [appWidth, appHeight, scale];
+    }
+
+    var modifier = new Modifier({
+        size: [640, 1136],
+        origin: [0.5, 0],
+        align: [0.5, 0]
+    });
+
+    function _resize(container, origW, origH){
+        appDims = getAppDims(origW, origH);
+        container.style.width = appDims[0] + "px";
+        container.style.height = appDims[1] + "px";
+
+        //modifier.setTransform(Transform.scale(appDims[2], appDims[2], 1));
+        //modifier.sizeFrom([640, undefined]);
+        var scale = appDims[2];
+        var trans = Transform.translate(0, 0, 0);
+        //var rotate = Transform.rotate(this.rotationX, this.rotationY, this.rotationZ);
+        var scale = Transform.scale(scale, scale, 1);
+
+        var mult = Transform.multiply(trans, scale);
+        modifier.transformFrom(mult);
+    }
+
+    function _init(origW, origH){
+        var contextContainer = document.getElementById("met-view");
+        //TODO: i would do this in CSS, and not call _resize on contextContainer
+        _resize(contextContainer, origW, origH);
+
+        //create the new one
+        context = Engine.createContext(contextContainer);
+        context.setPerspective(3000);
+
+        Engine.on("resize", function(){_resize(contextContainer);});
+        Engine.on("orientationchange", function(){_resize(contextContainer);});
+    }
+
+    function _loadApp(){
+
+    }
+
+
+
 
     //var stageView = new StageView();
     var director = new Director();
@@ -25,7 +82,7 @@ define(function(require, exports, module) {
 
     var pages = {};
 
-    Utility.loadURL("dataValue.json", function(data) {
+    function initApp(data) {
         // Check response
         if (!data) {
             return;
@@ -72,23 +129,19 @@ define(function(require, exports, module) {
             pageDesc: page
         });
 
-        var originModifier = new Modifier({
-            size:[max_page_width, max_page_height],
-            origin: [0.5, 0],
-            align: [0.5, 0]
-        });
-
         director.populateStage(pageView, page.nodes);
 
-        //mainContext.add(originModifier).add(pageView);
+
+
+        //context.add(modifier).add(pageView);
 
 
 
 
-        //var draggable = new Draggable();
-        //draggable.subscribe(pageView.scrollRecieverSurface);
-        //mainContext.add(originModifier).add(draggable).add(pageView);
-
+        ////var draggable = new Draggable();
+        ////draggable.subscribe(pageView.scrollRecieverSurface);
+        ////mainContext.add(originModifier).add(draggable).add(pageView);
+        //
         var pageViews = [];
 
         pageViews.push(pageView);
@@ -110,10 +163,10 @@ define(function(require, exports, module) {
         var scrollview = new Scrollview();
         scrollview.sequenceFrom(pageViews);
 
-        var contextSize = mainContext.getSize();
+        //var contextSize = context.getSize();
         var draggable = new Draggable({
                 xRange: [0, 0],
-                yRange: [- (max_page_height - contextSize[1]), 0]
+                yRange: [- (max_page_height - window.innerHeight), 0]
             }
         );
 
@@ -123,13 +176,16 @@ define(function(require, exports, module) {
 
         var originScrollviewModifier = new Modifier({
             size:[max_page_width, max_page_height],
-            origin: [0.5, 0],
-            align: [0.5, 0]
+            origin: [0, 0],
+            align: [0, 0]
         });
 
-        mainContext.add(originScrollviewModifier).add(draggable).add(scrollview);
+        // create the main context
+        _init(max_page_width, max_page_height);
+        context.add(modifier).add(draggable).add(scrollview);
+    }
 
-    }.bind(this));
+    Utility.loadURL("dataValue.json", initApp);
 
 
     //director.populateStageNew(stageView, nodeDescriptions, actionDescriptions);

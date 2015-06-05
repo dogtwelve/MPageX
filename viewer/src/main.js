610b16a052cd822faa6337c5c7ff634cf074f238
dogtwelve <x3191@hotmail.com>
2015-06-05 17:31:27 +0800

    auto-scale inside stageview

--- viewer/src/main.js
+++ viewer/src/main.js
@@ -23,21 +23,21 @@ define(function(require, exports, module) {
     var renderController = new RenderController();
     var draggable = new Draggable();
 
-    function getPageDims(context, origW, origH){
-        var size = context.getSize();
-        var scaleX = size[0] / origW;
-        var scaleY = size[1] / origH;
-
-        //here we are going to let the bottom of the screen be cut off to allow fit to more
-        //devices
-        var scale = Math.min(scaleX, scaleY);
-        //var scale = 1;
-
-        var pageWidth = origW * scale;
-        var pageHeight = origH * scale;
-
-        return [pageWidth, pageHeight, scale];
-    }
+    //function getPageDims(context, origW, origH){
+    //    var size = context.getSize();
+    //    var scaleX = size[0] / origW;
+    //    var scaleY = size[1] / origH;
+    //
+    //    //here we are going to let the bottom of the screen be cut off to allow fit to more
+    //    //devices
+    //    var scale = Math.min(scaleX, scaleY);
+    //    //var scale = 1;
+    //
+    //    var pageWidth = origW * scale;
+    //    var pageHeight = origH * scale;
+    //
+    //    return [pageWidth, pageHeight, scale];
+    //}
 
     var modifier = new Modifier({
         origin: [0.5, 0],
@@ -176,15 +176,15 @@ define(function(require, exports, module) {
         scrollview.sequenceFrom(pageViews);
 
 
-        draggable.setOptions({
-                xRange: [0, 0],
-                yRange: [- (max_page_height - viewPortSize[1]), 0]
-            }
-        );
-
-        for(var pageView in pageViews) {
-            draggable.subscribe(pageViews[pageView].scrollRecieverSurface);
-        }
+        //draggable.setOptions({
+        //        xRange: [0, 0],
+        //        yRange: [- (max_page_height - viewPortSize[1]), 0]
+        //    }
+        //);
+        //
+        //for(var pageView in pageViews) {
+        //    draggable.subscribe(pageViews[pageView].stageBgSurface);
+        //}
 
         //var originScrollviewModifier = new Modifier({
         //    size:[max_page_width, max_page_height],
@@ -194,10 +194,10 @@ define(function(require, exports, module) {
 
 
 
-        appDims = getPageDims(context, max_page_width, max_page_height);
-        modifier.sizeFrom([max_page_width, undefined]);
-
-        modifier.transformFrom(Transform.scale(appDims[2], appDims[2], 1));
+        //appDims = getPageDims(context, max_page_width, max_page_height);
+        //modifier.sizeFrom([max_page_width, undefined]);
+        //
+        //modifier.transformFrom(Transform.scale(appDims[2], appDims[2], 1));
 
         renderController.show(scrollview, {
                 duration: 0
@@ -207,7 +207,7 @@ define(function(require, exports, module) {
     _init();
     // create the main context
     _resize();
-    context.add(modifier).add(draggable).add(renderController);
+    context.add(renderController);
     Utility.loadURL("dataValue.json", initApp);
 
 

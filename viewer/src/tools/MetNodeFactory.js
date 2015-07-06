26ba94a0882d7a22db70feccb410da9404545234
dogtwelve <x3191@hotmail.com>
2015-07-06 18:01:36 +0800

    TODO:using svg for compatibility

--- viewer/src/tools/MetNodeFactory.js
+++ viewer/src/tools/MetNodeFactory.js
@@ -77,6 +77,7 @@ define(function(require, exports, module) {
                     classes: classes
                 });
             } else {
+                //TODO:using svg for compatibility
                 newSurface = new CanvasSurface({
                     size: size,
                     classes: classes,
